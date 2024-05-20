9ff84e33145654cbefccd47008dfd603328a77e7
EvertonHimself <evertondeoliveira@gmail.com>
2024-05-20 13:51:12 -0300

    Criada a classe Estudante.cs.
    Criada a classe Estudante.cs e o método inserirEstudante(...).

---
+++ GestorDeEstudantesT7/Estudante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstudantesT7
+{
+    internal class Estudante
+    {
+        MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
+
+        public bool inserirEstudante(string nome, string sobrenome, DateTime nascimento, 
+            string telefone, string genero, string endereco, MemoryStream foto)
+        {
+            return true;
+        }
+    }
+}
