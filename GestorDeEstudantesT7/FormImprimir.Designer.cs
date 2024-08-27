b54fdef3d266853ef4d9f9ac259d890ce8c47c5d
OmarFLK <167125795+OmarFLK@users.noreply.github.com>
2024-08-27 13:49:15 -0300

    Aula 27/08
    atrasei um dia pq ontem tive q sair mais cedo mas hoje eu fiz o formImprimir e comecei a programação dele.

---
+++ GestorDeEstudantesT7/FormImprimir.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GestorDeEstudantesT7
+{
+    partial class FormImprimir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFiltrar = new System.Windows.Forms.Button();
+            this.dataGridViewListaDeAlunos = new System.Windows.Forms.DataGridView();
+            this.buttonSalvarEmArquivoDeTexto = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonFeminino = new System.Windows.Forms.RadioButton();
+            this.radioButtonMasculino = new System.Windows.Forms.RadioButton();
+            this.radioButtonTodos = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonSim = new System.Windows.Forms.RadioButton();
+            this.radioButtonNao = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListaDeAlunos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonFiltrar
+            // 
+            this.buttonFiltrar.Location = new System.Drawing.Point(545, 12);
+            this.buttonFiltrar.Name = "buttonFiltrar";
+            this.buttonFiltrar.Size = new System.Drawing.Size(243, 64);
+            this.buttonFiltrar.TabIndex = 1;
+            this.buttonFiltrar.Text = "Filtrar";
+            this.buttonFiltrar.UseVisualStyleBackColor = true;
+            this.buttonFiltrar.Click += new System.EventHandler(this.buttonFiltrar_Click);
+            // 
+            // dataGridViewListaDeAlunos
+            // 
+            this.dataGridViewListaDeAlunos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewListaDeAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewListaDeAlunos.Location = new System.Drawing.Point(12, 82);
+            this.dataGridViewListaDeAlunos.Name = "dataGridViewListaDeAlunos";
+            this.dataGridViewListaDeAlunos.Size = new System.Drawing.Size(776, 293);
+            this.dataGridViewListaDeAlunos.TabIndex = 41;
+            // 
+            // buttonSalvarEmArquivoDeTexto
+            // 
+            this.buttonSalvarEmArquivoDeTexto.Location = new System.Drawing.Point(12, 381);
+            this.buttonSalvarEmArquivoDeTexto.Name = "buttonSalvarEmArquivoDeTexto";
+            this.buttonSalvarEmArquivoDeTexto.Size = new System.Drawing.Size(776, 64);
+            this.buttonSalvarEmArquivoDeTexto.TabIndex = 42;
+            this.buttonSalvarEmArquivoDeTexto.Text = "Salvar Em Arquivo de Texto";
+            this.buttonSalvarEmArquivoDeTexto.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.radioButtonTodos);
+            this.groupBox1.Controls.Add(this.radioButtonFeminino);
+            this.groupBox1.Controls.Add(this.radioButtonMasculino);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(527, 69);
+            this.groupBox1.TabIndex = 43;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButtonFeminino
+            // 
+            this.radioButtonFeminino.AutoSize = true;
+            this.radioButtonFeminino.Location = new System.Drawing.Point(94, 29);
+            this.radioButtonFeminino.Name = "radioButtonFeminino";
+            this.radioButtonFeminino.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonFeminino.TabIndex = 1;
+            this.radioButtonFeminino.Text = "Feminino";
+            this.radioButtonFeminino.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMasculino
+            // 
+            this.radioButtonMasculino.AutoSize = true;
+            this.radioButtonMasculino.Checked = true;
+            this.radioButtonMasculino.Location = new System.Drawing.Point(15, 29);
+            this.radioButtonMasculino.Name = "radioButtonMasculino";
+            this.radioButtonMasculino.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonMasculino.TabIndex = 0;
+            this.radioButtonMasculino.TabStop = true;
+            this.radioButtonMasculino.Text = "Masculino";
+            this.radioButtonMasculino.UseVisualStyleBackColor = true;
+            this.radioButtonMasculino.CheckedChanged += new System.EventHandler(this.radioButtonMasculino_CheckedChanged);
+            // 
+            // radioButtonTodos
+            // 
+            this.radioButtonTodos.AutoSize = true;
+            this.radioButtonTodos.Location = new System.Drawing.Point(167, 29);
+            this.radioButtonTodos.Name = "radioButtonTodos";
+            this.radioButtonTodos.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonTodos.TabIndex = 2;
+            this.radioButtonTodos.Text = "Todos";
+            this.radioButtonTodos.UseVisualStyleBackColor = true;
+            this.radioButtonTodos.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.radioButtonSim);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.radioButtonNao);
+            this.groupBox2.Location = new System.Drawing.Point(228, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(299, 69);
+            this.groupBox2.TabIndex = 44;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButtonSim
+            // 
+            this.radioButtonSim.AutoSize = true;
+            this.radioButtonSim.Location = new System.Drawing.Point(251, 16);
+            this.radioButtonSim.Name = "radioButtonSim";
+            this.radioButtonSim.Size = new System.Drawing.Size(42, 17);
+            this.radioButtonSim.TabIndex = 1;
+            this.radioButtonSim.Text = "Sim";
+            this.radioButtonSim.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNao
+            // 
+            this.radioButtonNao.AutoSize = true;
+            this.radioButtonNao.Checked = true;
+            this.radioButtonNao.Location = new System.Drawing.Point(200, 16);
+            this.radioButtonNao.Name = "radioButtonNao";
+            this.radioButtonNao.Size = new System.Drawing.Size(45, 17);
+            this.radioButtonNao.TabIndex = 0;
+            this.radioButtonNao.TabStop = true;
+            this.radioButtonNao.Text = "Não";
+            this.radioButtonNao.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Usar intervalo de datas:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(119, 43);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(84, 20);
+            this.dateTimePicker1.TabIndex = 45;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(209, 43);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(84, 20);
+            this.dateTimePicker2.TabIndex = 46;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Nascidos Entre:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // FormImprimir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 454);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSalvarEmArquivoDeTexto);
+            this.Controls.Add(this.dataGridViewListaDeAlunos);
+            this.Controls.Add(this.buttonFiltrar);
+            this.Name = "FormImprimir";
+            this.Text = "FormImprimir";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListaDeAlunos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonFiltrar;
+        private System.Windows.Forms.DataGridView dataGridViewListaDeAlunos;
+        private System.Windows.Forms.Button buttonSalvarEmArquivoDeTexto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.RadioButton radioButtonFeminino;
+        internal System.Windows.Forms.RadioButton radioButtonMasculino;
+        internal System.Windows.Forms.RadioButton radioButtonTodos;
+        private System.Windows.Forms.GroupBox groupBox2;
+        internal System.Windows.Forms.RadioButton radioButtonSim;
+        internal System.Windows.Forms.RadioButton radioButtonNao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ GestorDeEstudantesT7/FormImprimir.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorDeEstudantesT7
+{
+    public partial class FormImprimir : Form
+    {
+        public FormImprimir()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFiltrar_Click(object sender, EventArgs e)
+
+        {
+            //filtra os dados q serao exibidos
+            // MySqlCommand comando;
+            //string busca;
+            //verificar se o usuario quer usar um intervalo de datas
+
+            //if(radio button SIM.checked == true)
+            //{
+            //
+            //string dataInicial = dateTimePickerDataInicial.Value.ToString("dd-MM-yyyy");
+            //String dataFinal = dateTimePickerDataFinal.Value.ToString("dd-MM-yyyy");
+            //
+            //  if(radioButtonMasculino.checked)
+            //  {
+            //  busca = "SELECT * FROM ´estudantes´ WHERE ´nascimento´ BETWEEN´"
+            //  +dataInicial + "´AND´"
+            //  +dataFinal + "´AND genero = ´masculino´´"
+            //  }
+            //}
+            //
+            //comando = new MySqlCommand(busca);
+            //preencherTabela(comando);
+        }
+
+        private void radioButtonMasculino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
